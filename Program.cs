f8e68f70fb88f7cf4112691dbbda1671deb45972
Mariam Ahmed <114242076+mariamahmed17@users.noreply.github.com>
2022-12-17 22:44:34 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your start: ");
+            int start = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("please enter the end: ");
+            int end = int.Parse(Console.ReadLine());
+            Console.WriteLine("Prime numbers are :");
+
+            int i;
+            for (i = start; i <= end; i++)
+            {
+                bool primenumber = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        primenumber = false;
+                        break;
+                    }
+                }
+                if (primenumber)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+        }
+    }
+}
